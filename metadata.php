22bd109edfd93388ddd7f23968edd1a4da48bbca
[OXID-PS] Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2019-01-02 16:28:22 +0100

    Update metadata.php

--- metadata.php
+++ metadata.php
@@ -40,7 +40,7 @@ $aModule = [
         'en' => 'Tools to export, backup and import OXID eShop modules configuration data.',
     ],
     'thumbnail'   => 'out/pictures/oxpsmodulesconfig.png',
-    'version'     => '5.2.1',
+    'version'     => '6.0.0',
     'author'      => 'OXID Professional Services',
     'url'         => 'http://www.oxid-esales.com',
     'email'       => 'info@oxid-esales.com',
